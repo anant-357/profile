7f353ed2c80f63c8b4f01e725104b01c03decc69
anant-357 <anantsh.88@gmail.com>
2024-04-15 15:00:24 +0530

    animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const data_template = `{{#data}}
+const work_template = `{{#data}}
             <div class="content-item">
               <div class="content-header">
           <div class="content-title">{{title}}</div>
@@ -22,6 +22,25 @@ const gallery_template = ` {{#gallery}}
       </div>
         {{/gallery}}`;
 
+const home_template = `
+<div class="container">
+    <div class="lines" id="l-1"></div>
+    <div class="lines" id="l-2"></div>
+    <div class="lines" id="l-3"></div>
+    <div class="lines" id="l-4"></div>
+    <div class="lines" id="l-5"></div>
+    <div class="lines" id="l-6"></div>
+    <div class="lines" id="l-7"></div>
+    <div class="lines" id="l-8"></div>
+    <div class="lines" id="l-9"></div>
+    <div class="lines" id="l-10"></div>
+    <div class="lines" id="l-11"></div>
+    <div class="lines" id="l-12"></div>
+    <div class="lines" id="l-13"></div>
+    <div class="lines" id="l-14"></div>
+</div>
+`;
+
 if (document.URL.match("gallery")) {
   fetch(
     "https://raw.githubusercontent.com/anant-357/profile/main/content/gallery.json",
@@ -36,8 +55,11 @@ if (document.URL.match("gallery")) {
     "https://raw.githubusercontent.com/anant-357/profile/main/content/work.json",
   ).then((res) => {
     res.json().then((data) => {
-      const rendered = Mustache.render(data_template, { data });
+      const rendered = Mustache.render(work_template, { data });
       document.getElementById("page").innerHTML = rendered;
     });
   });
+} else {
+  const rendered = Mustache.render(home_template);
+  document.getElementById("page").innerHTML = rendered;
 }
